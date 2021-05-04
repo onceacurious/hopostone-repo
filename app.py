55b1a0e256420a3e889e223969986d7b81f69cc4
onceacurious <onceacurious@gmail.com>
2021-05-04 20:50:06 +0800

    Add python file and update some html

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+
+app =Flask(__name__)
+
+
+@app.route('/')
+def cover_page():
+    return render_template('cover.html')
+
+@app.route('/enquire')
+def enquiry_page():
+    return render_template('enquiry.html')
